28c83875b6180e2401d7de63252b22e39ff5e7d1
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-09-22 14:32:20 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ShtokerVN.Sprint1.Task0.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShtokerVN.Sprint1.Task0.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShtokerVN.Sprint1.Task0.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ab5879b-7b16-48b4-aa73-c45fba96d43c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
