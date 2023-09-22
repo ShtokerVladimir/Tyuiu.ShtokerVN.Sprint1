44c780c664460b5c297c2f240e9b7a13b7191611
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-09-22 19:59:36 +0500

    Task3

---
+++ Tyuiu.ShtokerVN.Sprint1.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShtokerVN.Sprint1.Task3.V12.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint1.Task3.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double wait = (x * y) / 2;
+            var res = ds.TriangleArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
