d2e9d9048261e4494b048e1af48e3bb7b3c74b60
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-09-22 18:18:55 +0500

    Task2

---
+++ Tyuiu.ShtokerVN.Sprint1.Task2.V4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShtokerVN.Sprint1.Task2.V4.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint1.Task2.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                  *");
+            Console.WriteLine("* Тема: Базовые навыки работы C#                                             *");
+            Console.WriteLine("* Задание #2                                                                 *");
+            Console.WriteLine("* Вариант #4                                                                 *");
+            Console.WriteLine("* Выполнил: Штокер Владимир Николаевич | ИИПБ-23-1                           *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,    *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.                *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            int x;
+
+            Console.WriteLine(" Введите значение стороны квадрата X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+         
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("Площадь квадрата X = " + ds.CalculateSquare(x));
+            Console.ReadLine();
+        }
+    }
+}
